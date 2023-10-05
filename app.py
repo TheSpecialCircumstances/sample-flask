9c4d85116232d3d81d3a735db1b8d94e0578f275
TheSpecialCircumstances <45333521+TheSpecialCircumstances@users.noreply.github.com>
2023-10-05 17:42:55 +0200

    Tsdfsdf

--- app.py
+++ app.py
@@ -7,3 +7,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return render_template("index.html")
+
+@app.route("/hi")
+def hi():
+    return '<h3>This is a Flask web application.</h3>'
